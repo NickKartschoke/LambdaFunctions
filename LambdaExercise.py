c47e35ee21d78c7f988e4c00ecbdf96e317bc935
NickKartschoke <Nkartschoke@dev-10.com>
2022-04-12 14:55:35 -0500

    weird type error still occuring

--- LambdaExercise.py
+++ LambdaExercise.py
@@ -1,6 +1,7 @@
 import csv
 from functools import reduce
 import statistics
+from tkinter import N
 
 filename = r'C:\Users\nickk\Downloads\DetroitCallsForService.csv'
 with open(filename, 'r') as csv_file:
@@ -27,9 +28,9 @@ dispatchtimeList = list(map(lambda x: x['dispatchtime'], cleanList))
 responsetimeList = list(map(lambda x: x['totalresponsetime'], cleanList))
 totaltimeList = list(map(lambda x: x['totaltime'], cleanList))
 
-print(f"{round(statistics.mean(dispatchtimeList),2)} is the average dispatch time.")
-print(f"{round(statistics.mean(responsetimeList),2)} is the average response time.")
-print(f"{round(statistics.mean(totaltimeList),2)} is the average total time.")
+#print(f"{round(statistics.mean(dispatchtimeList),2)} is the average dispatch time.")
+#print(f"{round(statistics.mean(responsetimeList),2)} is the average response time.")
+#print(f"{round(statistics.mean(totaltimeList),2)} is the average total time.")
 
 dispatchtimeTot = reduce(lambda num1, num2: num1+num2, dispatchtimeList, 0)
 avgDispatchTime = dispatchtimeTot/len(dispatchtimeList)
@@ -42,3 +43,28 @@ print(avgresponseTime)
 totaltimeTot = reduce(lambda num1, num2: num1+num2, totaltimeList, 0)
 avgtotalTime = totaltimeTot/len(totaltimeList)
 print(avgtotalTime)
+
+towns = list(map(lambda x:x['neighborhood'], cleanList))
+neighborhoods = list(set(towns))
+
+neighborhoodDict = {}
+
+for i in neighborhoods:
+    neighborhoodDict[i] = list(filter(lambda x: x['neighborhood'] == i, cleanList))
+
+print(neighborhoodDict)
+for n in neighborhoodDict.keys():
+    n_dispatchTimeList = list(map(lambda x: x['dispatchtime'], n))
+    n_responseTimeList = list(map(lambda x: x['totalresponsetime'], n))
+    n_totalTimeList = list(map(lambda x: x['totaltime'], n))
+    n_dispatchtimeTot = reduce(lambda num1, num2: num1+num2, dispatchtimeList, 0)
+    n_responsetimeTot = reduce(lambda num1, num2: num1+num2, responsetimeList, 0)
+    n_totaltimeTot = reduce(lambda num1, num2: num1+num2, totaltimeList, 0)
+    n_avgDispatch = n_dispatchtimeTot/len(n_dispatchTimeList)
+    n_avgResponse = n_responsetimeTot/len(n_responseTimeList)
+    n_avgTotal = n_totaltimeTot/len(n_totalTimeList)
+
+    print(f"neighborhood: {n} average dispatch time: {n_avgDispatch}")
+    print(f"neighborhood: {n} average response time: {n_avgResponse}")
+    print(f"neighborhood: {n} average total time: {n_avgTotal}")
+    print("-----------------------------------------------------------")
